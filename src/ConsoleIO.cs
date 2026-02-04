ad7a97f59cfd004133cd23ce958758fb645a18f8
Ofek Dory <ofeking475@gmail.com>
2026-02-04 15:02:51 +0200

    Finished the ConsoleIO class, containing input validation, and easy board output

--- src/ConsoleIO.cs
+++ src/ConsoleIO.cs
@@ -8,14 +8,33 @@ public class ConsoleIO
     }
 
     public Board parseBoard() {
- 
+        string boardInput = ReadInput();
+
+    
+        int [,] grid = new int[9,9];
+
+        for (int i = 0; i < 81; i++) {
+            char ch = boardInput[i];
+
+            if (ch < '0' || ch > '9') {
+                throw new ArgumentException("Input must contain only digits.");
+            }
+
+            int row = i / 9;
+            int col = i % 9;
+
+            grid[row, col] = ch - '0';
+        }
+
+        return new Board(grid); 
+        // If the input has logic issues, the board enforces all rules and will throw an exception
     } 
 
-    public bool ValidateBoard() {
-
-    }
-
     public void PrintBoard(IBoard board) {
-
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) 
+                Console.Write(board.GetCell(i, j));
+            Console.WriteLine();
+        }
     }
 }
