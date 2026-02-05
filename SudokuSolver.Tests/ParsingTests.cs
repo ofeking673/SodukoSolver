617b21f072b4323beb6e686a2cdd2c5257138618
Ofek Dory <ofeking475@gmail.com>
2026-02-05 15:32:25 +0200

    Changed test names to keep it uniform

--- SudokuSolver.Tests/ParsingTests.cs
+++ SudokuSolver.Tests/ParsingTests.cs
@@ -9,7 +9,7 @@ public class ParsingTests
     [InlineData("112345678900000000000000000000000000000000000000000000000000000000000000000000000")] // Duplicate 1 in first row
     [InlineData("123456789123000000000000000000000000000000000000000000000000000000000000000000000")] // Duplicate 1 in first col
     [InlineData("123456189000123000000000000000000000000000000000000000000000000000000000000000000")] // Duplicate 1 in top left box
-    public void Constructor_InvalidBoard_ThrowsException(string invalid)
+    public void Constructor_Invalid_Board_ThrowsException(string invalid)
     {
         int [,] grid = ConsoleIO.parseBoard(invalid);
         Assert.Throws<ArgumentException>(() => new Board(grid));
@@ -19,7 +19,7 @@ public class ParsingTests
     [InlineData("12345678X000000000000000000000000000000000000000000000000000000000000000000000000")]
     [InlineData("12345678000000000000000000000000000000000000000000000000000l000000000000000000000")]
     [InlineData("12345678000000000000000000000000000000000000000000000000000000000000000000000000j")]
-    public void Char_In_Board_ThrowException(string invalid)
+    public void Char_In_Board_ThrowsException(string invalid)
     {
         Assert.Throws<ArgumentException>(() => ConsoleIO.parseBoard(invalid));
     }
