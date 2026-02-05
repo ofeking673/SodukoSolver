ff843c05fc57659cca74eddfda125bede52b3094
Ofek Dory <ofeking475@gmail.com>
2026-02-05 17:16:43 +0200

    Integrated the use of backtrackingBoard in main to measure time spent on solve

--- SudokuSolver/src/Program.cs
+++ SudokuSolver/src/Program.cs
@@ -1,11 +1,24 @@
-using System;
+using System.Diagnostics;
 
 while (true)
 {
+    Solver solver = new Solver();
     try {
         int [,] grid = ConsoleIO.parseBoard(ConsoleIO.ReadInput());
-        var board = new Board(grid);
+        var board = new BacktrackingBoard(grid);
+
+        Console.WriteLine("Original board:");
         ConsoleIO.PrintBoard(board);
+
+        Console.WriteLine("-=========================-");
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        solver.Solve(board);
+        sw.Stop();
+        ConsoleIO.PrintBoard(board);
+
+        Console.WriteLine($"It took exactly {sw.ElapsedMilliseconds} ms.");
     }
     catch (Exception ex) {
         Console.WriteLine(ex.Message);
