eb22c2be5935d0545bc0fb52f403b6db555c88f4
ofeking673 <ofeking475@gmail.com>
2026-02-14 11:29:19 +0200

    Added XML Documentation for the BacktrackSolver class

--- SudokuSolver/src/Solvers/BacktrackSolver.cs
+++ SudokuSolver/src/Solvers/BacktrackSolver.cs
@@ -1,12 +1,21 @@
 public class BacktrackSolver : ISolver
 {
+  /// <summary>Solves the given Sudoku board using backtracking</summary>
+  /// <param name="board">The board to be solved</param>
+  /// <returns>True if the board was solved successfully, false otherwise</returns>
+  /// <exception cref="ArgumentException">Thrown if the provided board does not implement IListCandidatesBoard</exception>
   public bool Solve(IBoard board) {
     if (board is not IListCandidatesBoard listBoard)
       throw new ArgumentException("BacktrackSolver requires an IListCandidatesBoard instace.");
 
     return SolveRecursive(listBoard, 0, 0);
   } 
-
+  
+  /// <summary>Recursively attempts to solve the board using backtracking</summary>
+  /// <param name="board">The board to be solved</param>
+  /// <param name="row">The current row being processed</param>
+  /// <param name="col">The current column being processed</param>
+  /// <returns>True if the board was solved successfully, false otherwise</returns>
   private bool SolveRecursive(IListCandidatesBoard board, int row, int col)
   {
     int nextRow = row, nextCol = col + 1;
